198006e88d01db5415381eabb5549a9db2c2db7c
zimekk <zimekk@gmail.com>
2023-05-22 21:06:09 +0200

    feat(api): handle images

--- packages/api/src/audio.ts
+++ packages/api/src/audio.ts
@@ -11,23 +11,37 @@ const cwd = resolve(dirname(require.resolve("../../../.env")), LIBRARY_PATH);
 console.log({ LIBRARY_PATH, cwd });
 
 export const stream = () =>
-  Router().get("/:name?", async ({ headers, params }, res) => {
+  Router().get("/:name?", async ({ headers, params }, res, next) => {
     const { name } = params;
     if (name) {
-      // https://medium.com/@yelee2369/node-js-streaming-audio-files-10dd5e8670d0
-      const filepath = `${cwd}/${Array.isArray(name) ? name[0] : name}`;
-      const { size } = statSync(filepath);
-      const { range = "" } = headers;
-      const [start, end] = (([_, start, end]) => [
-        Number(start),
-        end ? Number(end) : size - 1,
-      ])(range.match(/bytes=(\d+)-(\d*)/) || []) as [number, number];
-      Object.entries({
-        "Content-Type": mime.lookup(filepath),
-        "Content-Length": end - start + 1,
-        "Content-Range": `bytes ${start}-${end}/${size}`,
-      }).forEach(([name, value]) => res.setHeader(name, value));
-      createReadStream(filepath, { start, end }).pipe(res.status(206));
+      try {
+        // https://medium.com/@yelee2369/node-js-streaming-audio-files-10dd5e8670d0
+        const filepath = `${cwd}/${Array.isArray(name) ? name[0] : name}`;
+        const { size } = statSync(filepath);
+        const { range = "" } = headers;
+        const [start, end] = (([_, start, end]) => [
+          Number(start),
+          end ? Number(end) : size - 1,
+        ])(range.match(/bytes=(\d+)-(\d*)/) || []) as [number, number];
+        Object.entries(
+          Object.assign(
+            ((type) => (type ? { "Content-Type": mime.lookup(filepath) } : {}))(
+              mime.lookup(filepath)
+            ),
+            !isNaN(start)
+              ? {
+                  "Content-Length": `${end - start + 1}`,
+                  "Content-Range": `bytes ${start}-${end}/${size}`,
+                }
+              : {}
+          )
+        ).forEach(([name, value]) => value && res.setHeader(name, value));
+        createReadStream(filepath, !isNaN(start) ? { start, end } : {}).pipe(
+          res.status(isNaN(start) ? 200 : 206)
+        );
+      } catch (e) {
+        next(e);
+      }
     } else {
       res.status(200).json(
         sync("**/*.{flac,mp3,wav}", {
