dd7c782b464bd69ee0a6f78f957a7f5bf6ddfcfe
Marcin Makarewicz <marcin@makarewicz.eu>
2023-09-02 19:07:12 +0200

    feat(worker): chunked parser

--- packages/worker/src/parser/index.ts
+++ packages/worker/src/parser/index.ts
@@ -1,95 +1,30 @@
-import { parse } from "../client";
+import chunk from "chunk";
+import { chrome, parse } from "../client";
 
 export const parser = async (
   url = "https://api.um.warszawa.pl/api/action/dbstore_get/?id=ab75c33d-3a26-4342-b36a-6e5fef0a3ac3"
 ) => {
   console.log(["parser"], { url });
 
-  const returnvalue = {
-    url,
-    json: {
-      result: [
-        {
-          values: [
-            {
-              value: "1001",
-              key: "zespol",
-            },
-            {
-              value: "03",
-              key: "slupek",
-            },
-            {
-              value: "Kijowska",
-              key: "nazwa_zespolu",
-            },
-            {
-              value: "2201",
-              key: "id_ulicy",
-            },
-            {
-              value: "52.248928",
-              key: "szer_geo",
-            },
-            {
-              value: "21.044169",
-              key: "dlug_geo",
-            },
-            {
-              value: "al.Zieleniecka",
-              key: "kierunek",
-            },
-            {
-              value: "2023-05-02 00:00:00.0",
-              key: "obowiazuje_od",
-            },
-          ],
-        },
-        {
-          values: [
-            {
-              value: "1001",
-              key: "zespol",
-            },
-            {
-              value: "04",
-              key: "slupek",
-            },
-            {
-              value: "Kijowska",
-              key: "nazwa_zespolu",
-            },
-            {
-              value: "2201",
-              key: "id_ulicy",
-            },
-            {
-              value: "52.249969",
-              key: "szer_geo",
-            },
-            {
-              value: "21.041588",
-              key: "dlug_geo",
-            },
-            {
-              value: "Ząbkowska",
-              key: "kierunek",
-            },
-            {
-              value: "2023-05-02 00:00:00.0",
-              key: "obowiazuje_od",
-            },
-          ],
-        },
-      ],
-    },
-  };
+  const { json, ...rest } = await chrome(url);
 
-  console.log(
-    await parse({
-      id: url,
-      data: { url },
-      returnvalue,
-    })
+  await chunk(json.result.slice(0, 10_000), 1_000).reduce(
+    (promise, result, key, list) =>
+      promise
+        .then(() => ({
+          ...rest,
+          json: {
+            result,
+          },
+        }))
+        .then((returnvalue) =>
+          (console.log(`${key + 1}/${list.length}`, { returnvalue }),
+          parse({
+            id: url,
+            data: { url },
+            returnvalue,
+          })).then(console.log)
+        ),
+    Promise.resolve()
   );
 };
