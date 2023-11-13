05e3ccc34f567d8bff2273eb3b4e8f385d3964b1
Marcin Makarewicz <marcin@makarewicz.eu>
2023-11-14 00:14:06 +0100

    feat(video): add videos

--- packages/video/src/containers/index.tsx
+++ packages/video/src/containers/index.tsx
@@ -79,11 +79,18 @@ function List() {
     "https://www.youtube.com/watch?v=XXYlFuWEuKI",
     "https://www.youtube.com/watch?v=a0q6JMuLBYQ",
     "https://www.youtube.com/watch?v=b6U3xeYqdKQ",
+    "https://www.youtube.com/watch?v=4qvBnEgCvkc",
+    "https://www.youtube.com/watch?v=m8_8tMzmpTg",
+    "https://www.youtube.com/watch?v=j4IXCjWnAkY",
+    "https://www.youtube.com/watch?v=gCadlN8fexk",
+    "https://www.youtube.com/watch?v=aAkMkVFwAoo",
+    "https://www.youtube.com/watch?v=E8gmARGvPlI",
     "https://www.youtube.com/watch?v=L4QxmPMplYc",
     "https://www.youtube.com/watch?v=WJvtX-Ec1S8",
     "https://www.youtube.com/watch?v=wXWZiGnaeqo",
     "https://www.youtube.com/watch?v=OiC1rgCPmUQ",
     "https://www.youtube.com/watch?v=ekmHAfUIu1M",
+    "https://www.youtube.com/watch?v=YFoDEQXJLcQ",
     "https://www.youtube.com/watch?v=c8WDzsqQ7UY",
   ]);
 
