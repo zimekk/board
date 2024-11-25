b0e8f9d7292a05f63c43437d2b6d096fac6ba0fc
Marcin Makarewicz <marcin@makarewicz.eu>
2024-11-25 23:08:18 +0100

    fix(scrap): add records

--- packages/scrap/src/records.ts
+++ packages/scrap/src/records.ts
@@ -317,10 +317,12 @@ export const records = [
       // "audi/q8",
       "bmw/seria-5?search%5Bfilter_enum_generation%5D%5B0%5D=gen-g30-2017&search%5Bfilter_enum_generation%5D%5B1%5D=gen-g60-2023",
       "bmw/x3?search%5Bfilter_enum_generation%5D=gen-g01-2017",
+      "bmw/x3?search%5Bfilter_enum_generation%5D=gen-g45-2024",
       "honda/accord/seg-combi?search%5Bfilter_enum_generation%5D=gen-viii-2008",
       // "honda/jazz?search%5Bfilter_enum_generation%5D=gen-ii-2002-2008-jazz",
       "mini/cooper-s--john-cooper-works",
       "porsche/macan",
+      "volvo/ex90?search%5Border%5D=relevance_web",
       "volvo/xc-60?search%5Bfilter_enum_generation%5D=gen-ii-2017",
     ]
       .map((cat) => `https://www.otomoto.pl/osobowe/${cat}`)
