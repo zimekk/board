89323840b1ee2f2b7984e58072f0e5f02d71b3e4
Marcin Makarewicz <marcin@makarewicz.eu>
2024-11-26 23:01:26 +0100

    fix(scrap): add records

--- packages/scrap/src/records.ts
+++ packages/scrap/src/records.ts
@@ -499,6 +499,7 @@ export const records = [
       "g-6/c/1544-drukarki-etykiet.html",
       "g-6/c/1749-projektory.html?f1809-technologia-wyswietlania=126474-laser&f1809-technologia-wyswietlania=157709-led",
       "g-6/c/1751-prezentery.html",
+      "g-6/c/2280-szafy-rack.html?producent=154-apc",
       "g-6/c/2280-szafy-rack.html?producent=1278-lanberg",
       "g-6/c/2326-kamery-ip.html?producent=276-tp-link&producent=2287-hikvision",
       "g-6/c/2506-glosniki-przenosne.html?producent=374-jbl&f1872-moc-glosnikow-rms=71075-10-49-w",
