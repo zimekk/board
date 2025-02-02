11c1c9d462f87a49375d7d9734a528e8632bd44d
Marcin Makarewicz <marcin@makarewicz.eu>
2025-02-02 23:45:25 +0100

    feat(api): live m3u

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,6 @@
-import { Router } from "express";
-import cors from "cors";
 import { query } from "@dev/sql";
+import cors from "cors";
+import { Router } from "express";
 
 export const api = Router()
   .use(
@@ -9,6 +9,16 @@ export const api = Router()
     }),
   )
   .all("/", (_req, res) => res.json({ hello: "Hello" }))
+  .all("/live.m3u", (req, res) => {
+    const stream = "NZXT";
+    return res.send(
+      (({ hostname }) => `#EXTM3U
+  #EXTINF:-1, ${stream} RTMP SERVER
+  rtmp://${hostname}/live/${stream}`)(
+        new URL(`http://${req.headers.host}` || process.env.MEDIA_URL),
+      ),
+    );
+  })
   .all("/data.json", (_req, res) =>
     query(`select * from contact`).then(({ rows }) => res.json(rows)),
   )
