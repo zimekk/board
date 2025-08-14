9e91e9c4273cd343eb6c165de2ff5635b8510a18
zimekk <zimekk@users.noreply.github.com>
2025-08-14 13:29:15 +0000

    deploy: 01e01999c954ad0fccefbf450dca750e674eb6ec

---
+++ 387.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_dev_web=self.webpackChunk_dev_web||[]).push([[387],{58387:(e,t,r)=>{r.r(t),r.d(t,{default:()=>n});var a=r(22155);function n(){return a.createElement("section",null,a.createElement("h2",null,"LLama"),a.createElement("a",{href:"https://withcatai.github.io/node-llama-cpp/guide/chat-prompt-wrapper",target:"_blank",rel:"noopener noreferrer"},"guide/chat-prompt-wrapper"))}}}]);
+//# sourceMappingURL=387.js.map
